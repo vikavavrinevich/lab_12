9ff28b377efb3cfd449d7e7fd2e31acf0928ca2f
vikavavrinevich <viktoriya.vavrinevich@gmail.com>
2024-04-20 18:12:14 +0300

    commit 12

---
+++ script.js
@@ -0,0 +1,70 @@
+/* alert('Vavrinevich')
+let myAge = 17;
+let message = 'hello 17'
+console.log(myAge, message) */
+
+
+
+
+
+
+/* let objects = {
+    message: 'nice',
+    number: 99,
+    boolean: true,
+    undef: undefined,
+    zero: null
+}
+ */
+
+
+
+
+
+/* let isAdult = confirm('Ти повнолытній?')
+console.log(isAdult
+
+) */
+
+
+
+
+
+/* let myName = 'Vika'
+let mySurname = 'Vavrinevich'
+let myGroup = 'KN-321'
+let years = 2006
+let merry = false
+console.log(typeof myName, typeof mySurname, typeof myGroup, typeof years, typeof merry)
+console.log(years, merry, myGroup, myName, mySurname) */
+
+
+
+
+/* let zero = null
+let undef = undefined
+console.log(typeof zero, typeof undef) */
+
+
+
+/* let login = prompt('write you login')
+let mail = prompt('write you mail')
+let password = prompt('write you password')
+alert(`Dear ${login}, your email is ${mail}, your password is ${password}`) */
+
+
+
+
+// Обчислення секунд у годині
+const secondsPerHour = 60 * 60; // 60 секунд * 60 хвилин
+
+// Обчислення секунд у добі
+const secondsPerDay = secondsPerHour * 24; // 24 години
+
+// Обчислення секунд у середньому місяці
+const secondsPerMonth = secondsPerDay * 30; // 30 днів в середньому місяці
+
+// Вивід результатів
+console.log(`Секунд в одній годині: ${secondsPerHour}`);
+console.log(`Секунд в одній добі: ${secondsPerDay}`);
+console.log(`Секунд в середньому місяці: ${secondsPerMonth}`);
